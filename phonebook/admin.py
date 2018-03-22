1b3b4096889d88296af5253420d4f4542942dc8f
smartankur4u <ankurnarkhede@gmail.com>
2018-03-23 01:02:07 +0530

    modified model names

--- phonebook/admin.py
+++ phonebook/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import names, phone, email
 
-admin.site.register(names,phone,email)
+admin.site.register(names)
+admin.site.register(phone)
+admin.site.register(email)
 
--- phonebook/models.py
+++ phonebook/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
 
 
 class names(models.Model):
     name=models.CharField(max_length=100, null=True)
+    owner=models.ForeignKey(User)
     status=models.BooleanField(default=True)
     created_at=models.DateTimeField(auto_now_add=True)
 
@@ -12,8 +16,7 @@ class names(models.Model):
         return self.name
 
 class phone(models.Model):
-    names_id=models.ForeignKey('names')
-    # names_id=models.CharField(max_length=20, null=True)
+    names_id=models.ForeignKey(names)
     phone_no=models.CharField(max_length=20, null=True)
     status=models.BooleanField(default=True)
     created_at=models.DateTimeField(auto_now_add=True)
@@ -23,7 +26,7 @@ class phone(models.Model):
         return self.phone_no
 
 class email(models.Model):
-    names_id=models.ForeignKey('names')
+    names_id=models.ForeignKey(names)
     email_id=models.CharField(max_length=100, null=True)
     status=models.BooleanField(default=True)
     created_at=models.DateTimeField(auto_now_add=True)
