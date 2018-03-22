561153330c9b36588bdf1d1a90e96b8a2e129a9e
smartankur4u <ankurnarkhede@gmail.com>
2018-03-23 01:26:13 +0530

    ...

--- phonebook/views.py
+++ phonebook/views.py
@@ -10,4 +10,26 @@ from django.http import JsonResponse
 from .models import names, phone, email
 from .serializers import names_serializer, phone_serializer, email_serializer
 
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
 # Create your views here.
+
+class names_list(APIView):
+
+    def get(self, request):
+
+        if request.user.is_authenticated ():
+            pass
+        else:
+            pass
+
+        # current_user=request.user
+        current_user=1
+        print('Current User is ',current_user)
+        user_phonebook=names.objects.get(owner=current_user)
+        serializer=names_serializer(user_phonebook)
+        return Response(serializer.data)
+
+
+
