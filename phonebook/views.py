025a4034aca5435852c690c50e4d3db3e37022bf
smartankur4u <ankurnarkhede@gmail.com>
2018-03-23 00:33:50 +0530

    modified controller

--- phonebook/views.py
+++ phonebook/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from django.shortcuts import get_object_or_404
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import JsonResponse
+
+from .models import names, phone, email
+from .serializers import names_serializer, phone_serializer, email_serializer
+
 # Create your views here.
